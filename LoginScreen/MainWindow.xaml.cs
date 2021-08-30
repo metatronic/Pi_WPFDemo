55381793c7c0ff3adeb02cce6ecef62fa1e4df4d
pran saha <iampransaha@gmail.com>
2021-08-30 23:54:51 +0530

    Add project files.

---
+++ LoginScreen/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginScreen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int passwordAttempt;
+        public MainWindow()
+        {
+            InitializeComponent();
+            passwordAttempt = 0;
+        }
+
+        private void Submit_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (userNameTxt.Text == "admin" && passwordTxt.Password == "123")
+            {
+                MessageBox.Show("Succesfull");
+                var window = new Registration();
+                window.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("incorrect username or password");
+                if (passwordAttempt < 3)
+                {
+                    userNameTxt.Text = "";
+                    passwordTxt.Password = "";
+                    passwordAttempt++;
+                }
+                else
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+        }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ LoginScreen/Registration.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoginScreen
+{
+    /// <summary>
+    /// Interaction logic for Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        string gender;
+        string status;
+        public Registration()
+        {
+            InitializeComponent();
+            gender = "";
+            status = "";
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (UserNameTxt.Text == "")
+            {
+                MessageBox.Show("Enter UserName");
+            }
+            else
+            {
+                string path = @"D:\Code\Users\" + UserNameTxt.Text + ".txt";
+                using (FileStream fileStream = new FileStream(path, FileMode.Create))
+                {
+                    StreamWriter sw = new StreamWriter(fileStream);
+                    sw.WriteLine($"User Name={UserNameTxt.Text}");
+                    sw.WriteLine($"Password={PasswordTxt.Password}");
+                    sw.WriteLine($"Address={AddressTxt.Text}");
+                    ComboBoxItem cb = (ComboBoxItem)CountryCombo.SelectedItem;
+                    sw.WriteLine($"Country={cb.Content}");
+                    sw.WriteLine($"Telephone={TelTxt.Text}");
+                    sw.WriteLine($"Mobile={MobTxt.Text}");
+                    sw.WriteLine($"Gender={gender}");
+                    sw.WriteLine($"Status={status}");
+                    string qualification = "";
+                    foreach (object item in QualificationListbox.SelectedItems)
+                    {
+                        ListBoxItem li = (ListBoxItem)item;
+                        qualification += li.Content + " ";
+                    }
+                    sw.WriteLine($"Qualification={qualification}");
+                    sw.Close();
+                }
+                UpdateFileList();
+                ShowRichText(path);
+            }
+
+        }
+
+        void ShowRichText(string path)
+        {
+            using (FileStream fileStream = new FileStream(path, FileMode.Open))
+            {
+                StreamReader sr = new StreamReader(fileStream);
+                string userContent = sr.ReadToEnd();
+                UserInfo.Visibility = Visibility.Visible;
+                UserInfo.Document.Blocks.Clear();
+                UserInfo.AppendText(userContent);
+            }
+        }
+
+        private void TelCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            TelTxt.IsEnabled = true;
+        }
+
+        private void MobCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            MobTxt.IsEnabled = true;
+        }
+
+        private void MobCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MobTxt.IsEnabled = false;
+        }
+
+        private void TelCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            TelTxt.IsEnabled = false;
+        }
+
+        private void GenderRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rd = (RadioButton)sender;
+            gender = rd.Content.ToString();
+        }
+
+        private void StatusRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rd = (RadioButton)sender;
+            status = rd.Content.ToString();
+        }
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            UserInfo.Visibility = Visibility.Hidden;
+        }
+
+        private void DisplayExistingCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            ExistingFilesListbox.Visibility = Visibility.Visible;
+            UpdateFileList();
+        }
+
+        private void DisplayExistingCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ExistingFilesListbox.Visibility = Visibility.Hidden;
+        }
+
+        void UpdateFileList()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(@"D:\Code\Users\");
+            FileInfo[] fi = directoryInfo.GetFiles();
+            ExistingFilesListbox.Items.Clear();
+            foreach (FileInfo item in fi)
+            {
+                ExistingFilesListbox.Items.Add(item.FullName);
+            }
+        }
+
+        private void ExistingFilesListbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string li = (sender as ListBox).SelectedItem as string;
+            if (li != null)
+            {
+                ShowRichText(li);
+            }
+        }
+    }
+}
